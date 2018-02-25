0ec2c0abed6c792e259630e440067c46d3d12991
wu.shen <wu.shen@qunar.com>
2018-02-25 15:08:56 +0800

    completer

---
+++ src/main/java/me/stamwoo/annotation/MyRequestParam.java
@@ -0,0 +1,14 @@
+package me.stamwoo.annotation;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyRequestParam {
+    /**
+     * 表示参数的别名，必填
+     * @return
+     */
+    String value();
+}
